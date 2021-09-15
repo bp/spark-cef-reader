8f566fd36bfc6c876c3dd3353ba982ec120c48de
Darren Fuller <darren@elastacloud.com>
2021-09-15 13:27:07 +0100

    clean up ResourceFileUtils

--- src/test/scala/com/bp/sds/cef/utils/ResourceFileUtils.scala
+++ src/test/scala/com/bp/sds/cef/utils/ResourceFileUtils.scala
@@ -1,11 +1,14 @@
 package com.bp.sds.cef.utils
 
-import java.net.URI
-import java.nio.file.Path
-
 import scala.io.{BufferedSource, Source}
 
 object ResourceFileUtils {
+  /**
+   * Gets the content of a test resource file as a string
+   *
+   * @param relativePath location of the test resource file
+   * @return a [[String]] containing the full content of the file
+   */
   def getFileContent(relativePath: String): String = {
     val scriptSourcePath = getClass.getResource(relativePath).getPath
     var scriptSource: BufferedSource = null
@@ -18,12 +21,14 @@ object ResourceFileUtils {
     }
   }
 
+  /**
+   * Gets the path of a test resource file
+   *
+   * @param relativePath path to the test resource file
+   * @return a full path name of the requested file
+   */
   def getFilePath(relativePath: String): String = {
     getClass.getResource(relativePath).getPath
-      .replace("%20", " ")  // Replace spaces in the name at this stage so as to not confuse the test case
-  }
-
-  def getResourceRoot: URI = {
-    getClass.getResource("/").toURI
+      .replace("%20", " ") // Replace spaces in the name at this stage so as to not confuse the test case
   }
 }
